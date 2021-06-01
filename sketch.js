d8105c436db39fbeb2177de6f9ee5162f97111d6
itsaryas23 <74662533+itsaryas23@users.noreply.github.com>
2021-06-01 23:10:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var mario, mario_img;
+var ground;
+var background;
+
+
+function preload(){
+
+}
+
+function setup(){
+   createCanvas(1287,720);
+   background('#5C94FB');
+
+   
+
+
+
+}
+
+function draw(){
+
+}
